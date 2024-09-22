353cf6e54e84ca43201db035fa67d4cb24dcbfaa
hermanpapko <herman.papko@gmail.com>
2024-09-22 19:53:32 +0200

    2.6

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,27 @@ var myImage = document.querySelector("img");
 myImage.onclick = function () {
   var mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Mozilla_Firefox_0.8_logo.svg/1920px-Mozilla_Firefox_0.8_logo.svg.png");
+    myImage.setAttribute("src", "images/firefox2.png");
   } else {
-    myImage.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/1/16/Firefox_logo%2C_2017.png");
+    myImage.setAttribute("src", "images/firefox-icon.png");
   }
 };
+
+var myButton = document.querySelector("button");
+var myHeading = document.querySelector("h1");
+
+function setUserName() {
+    var myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla is cool, " + myName;
+  }
+ if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla is cool, " + storedName;
+  }
+  myButton.onclick = function () {
+    setUserName();
+  };
+  
